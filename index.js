6370cbb51f4cd68f92507a8160be63ecf5fd2aa0
Tilde Ann Thurium <tilde@thuryism.net>
2019-07-26 16:00:01 -0700

    use process.env.port for heroku

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const getRandomAffirmation = require('./affirmations');
 
 const app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.get('/', (req, res) => {
   res.json({affirmation: getRandomAffirmation()});
 });
 
-http.createServer(app).listen(3000, () => {
-  console.log('express server listening on port 3000');
+http.createServer(app).listen(port, () => {
+  console.log(`express server listening on port ${port}`);
 });
