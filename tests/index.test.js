db94907056cfcd81de83fbc6ce7836c05c067902
kenessajr <jeanremy1806@gmail.com>
2020-03-27 02:52:49 +0200

    run test suites using jest and supertest

---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest")
+const server = require("../index")
+const affirmations = require('../affirmations')
+
+describe('GET Affirmation Endpoints', () => {
+  it('should return an affirmation', async () => {
+    const res = await request(server).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(affirmations).toContain(res.body['affirmation']);
+  });
+});
