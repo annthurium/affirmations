6d8b327a68774587c82e71438811bc93fb9bfc7c
surgeBinded <ropotdaniel@gmail.com>
2020-07-16 17:30:20 +0200

    enabled CORS

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const app = express();
 let PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({affirmation: getRandomAffirmation()});
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json({ affirmation: getRandomAffirmation() });
 });
 
-const server = app.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server is live at localhost:${PORT}`)
+);
 
 module.exports = server;
