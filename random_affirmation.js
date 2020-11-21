a0bf61f6e45141262e34918fe0cf3db29b5c6fce
Gust4voSales <manoel0gustavo@gmail.com>
2020-11-21 11:02:58 -0300

    feat: add lang query to the route

--- random_affirmation.js
+++ random_affirmation.js
@@ -1,4 +1,19 @@
-const affirmations = require('./affirmations')
+const {
+  affirmations, 
+  affirmations_ptBr,
+} = require('./affirmations')
 
-const getRandomAffirmation = () => affirmations[Math.floor(Math.random() * affirmations.length)];
+const getRandomAffirmation = (lang='en-us') => {
+  let affimationsList = [];
+
+  // If the lang is set to pt-br then use the Brazillian affirmations list
+  // the default lang is en-us
+  if (lang==='pt-br')
+    affimationsList = affirmations_ptBr; 
+  else 
+    affimationsList = affirmations; 
+  
+  return affimationsList[Math.floor(Math.random() * affimationsList.length)];
+};
+
 module.exports = getRandomAffirmation;
